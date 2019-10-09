7f1ba1108429fbbe889cf44ecf97f54ecece83b5
phenix1229 <paul.garay@codeimmersives.com>
2019-10-09 14:19:52 -0400

    starting

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
+function appendToUl(element){
+    const ul = document.querySelector('ul');
+    const element = document.querySelector('element');
+    ul.appendChild(element);
+}
+
+function appendToLorem(element){
+    const lorem = document.querySelector('#lorem');
+    const element = document.querySelector('element');
+    lorem.appendChild(element);
+}
+
+function newLi(text){
+    const newLi = document.createElement('li');
+    return newLi.innerText = text;
+}
+
+function appendImg(img, sourceStr){
+
+}
 
