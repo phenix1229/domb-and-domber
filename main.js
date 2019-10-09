984621e14cf461b3890ddaeec9102a01b9c798fb
phenix1229 <paul.garay@codeimmersives.com>
2019-10-09 15:14:28 -0400

    starting

--- main.js
+++ main.js
@@ -1,21 +1,34 @@
 function appendToUl(element){
     const ul = document.querySelector('ul');
-    const element = document.querySelector('element');
     ul.appendChild(element);
 }
 
 function appendToLorem(element){
     const lorem = document.querySelector('#lorem');
-    const element = document.querySelector('element');
     lorem.appendChild(element);
 }
 
-function newLi(text){
+function makeNewLi(newText){
     const newLi = document.createElement('li');
-    return newLi.innerText = text;
+    newLi.innerText = newText;
+    // return liText
 }
 
 function appendImg(img, sourceStr){
 
 }
 
+function assignClass(thing1, thing2){
+
+}
+
+function newElementAndID(tag, newID){
+
+}
+
+function strIDColor(colorStr, idStr){
+
+}
+
+text1 = makeNewLi('new li 1');
+appendToUl(text1);
