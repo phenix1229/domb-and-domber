cdc1d4a81f8b37105993fe1e6f889615bda48c90
phenix1229 <paul.garay@codeimmersives.com>
2019-10-09 21:17:28 -0400

    completed all functions and all tasks

--- main.js
+++ main.js
@@ -23,15 +23,18 @@ function addImg(img, sourceStr){
 }
 
 function assignClass(thing1, thing2){
-
+    const class1 = thing1.getAttribute('class')
+    thing2.setAttribute('class', class1);
 }
 
-function newElementAndID(tag, newID){
-
+function newElementAndID(tag, newId){
+    const newItem = document.createElement(tag);
+    newItem.setAttribute('id', newId);
+    return newItem;
 }
 
-function strIDColor(colorStr, idStr){
-
+function strIDColor(idStr, colorStr){
+    document.querySelector('#'+idStr).style.color = colorStr;
 }
 
 
@@ -42,3 +45,10 @@ appendToUl(makeNewLi('don\'t forget me'));
 
 appendToLorem(addImg('gh-logo', 'https://image.flaticon.com/icons/svg/25/25231.svg'))
 appendToLorem(addImg('ci-logo','https://www.multichannel.com/.image/t_share/MTU0MDYzODU3ODUyMDk4Mjk5/aws-logojpg.jpg'));
+
+const ol1 = document.querySelector('ol');
+const olLi =ol1.querySelectorAll('li');
+assignClass(olLi[0], olLi[1]);
+assignClass(olLi[0], olLi[2]);
+
+strIDColor('heading', 'lightGreen')
